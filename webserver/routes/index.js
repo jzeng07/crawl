487700a4788bf0556a58be7c1851ba641df76cc6
jzeng07 <jun.zeng.01@gmail.com>
2016-04-30 10:29:42 -0700

    first version with angularjs

--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -4,10 +4,10 @@ var homepage = require('models/home.js');
 var site = require('models/site.js');
 
 /* GET home page. */
-router.get('/', homepage);
+//router.get('/', homepage);
 
 /* GET article list of a site. */
-router.get('/:sitename', homepage);
-router.get('/:sitename/:pageid', homepage);
+router.get('/:sitename', site.homepage);
+router.get('/:sitename/:pageid', site.article);
 
 module.exports = router;
