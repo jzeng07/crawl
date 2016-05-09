3c31099c466e7c2cf6e7358ab8164e675494c94a
jzeng07 <jun.zeng.01@gmail.com>
2016-05-08 21:03:02 -0700

    restore public/index.html

--- webserver/js/controllers.js
+++
@@ -1,8 +0,0 @@
-var aggregate = angular.module('aggregate', []);
-
-aggregate.controller('articlesCtrl', function($scopt, $http){
-    $http.get('/wenxuecity').success(function(data) {
-        console.log(data);
-        $scope.articles = data;
-    });
-});
---
+++ webserver/public/js/controllers.js
@@ -0,0 +1,27 @@
+var aggregate = angular.module('aggregate', []);
+
+aggregate.controller('articlesCtrl', function($scope, $http, $sce){
+    $scope.go = function(path) {
+        console.log(path);
+        $http.get(path).success(function(data) {
+            console.log(data);
+            for (var i=0; i<data.articles.length; i++) {
+                data.articles[i].content = $sce.trustAsHtml(data.articles[i].content);
+            }
+            $scope.sites = data.sites;
+            $scope.articles = data.articles;
+            $scope.showlist = true;
+            $scope.showcontent = false;
+        });
+    }
+    $scope.read = function(path) {
+        console.log(path);
+        $http.get(path).success(function(data) {
+            console.log(data);
+            $scope.article = data;
+            $scope.showlist = false;
+            $scope.showcontent = true;
+            $scope.article.content = $sce.trustAsHtml(data.content);
+        });
+    }
+});
